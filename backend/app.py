41e22372c0521bb99fd66e5649a0af9d08f06e58
Sudha7670 <peruri.sudhakar@gmail.com>
2026-01-10 08:09:33 +0530

    Fix main check from '__main__' to '__master__'

--- backend/app.py
+++ backend/app.py
@@ -78,6 +78,6 @@ def admin_contacts():
     return html
 
 
-if __name__ == "__main__":
+if __name__ == "__master__":
     port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
